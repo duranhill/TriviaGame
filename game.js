6d56fef476dec9b21eb1d69852cfa43b67f77469
unknown <duranhill@gmail.com>
2017-12-30 19:17:43 -0700

    started

---
+++ game.js
@@ -0,0 +1,13 @@
+alert('Welcome to The Trivia Game!!')
+
+
+
+
+
+function check(){
+
+document.getElementById('after_submit').style.visibility = 'visible';
+document.getElementById('number_correct').innerHTML ='You got ' + correct + 'correct';
+}
+
+
